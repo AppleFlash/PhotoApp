e59c48b7a53c32f43fc4b9d105d63c6446d527e4
Vladislav <sedinkin1993@gmail.com>
2018-04-16 00:38:39 +0600

    Remove appearing image animation if image already set

--- PhotoApp/Presentations/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoApp/Presentations/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -13,29 +13,29 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet private weak var photoImage: UIImageView!
     @IBOutlet private weak var spinner: UIActivityIndicatorView!
     
-    var photo: Photo? {
-        didSet {
-            updateImageView()
-        }
+    var isHaveImage: Bool {
+        return photoImage.image != nil
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        updateImageView()
+        updateImageView(with: nil, animated: true)
     }
     
-    func updateImageView() {
-        if let image = photo?.smallImage {
+    func updateImageView(with image: UIImage?, animated: Bool) {
+        if let image = image {
             photoImage.image = image
-            photoImage.alpha = 0
-            UIView.animate(withDuration: 0.2, animations: {
-                self.photoImage.alpha = 1.0
-                self.spinner.alpha = 0
-            }, completion: { _ in
-                self.spinner.stopAnimating()
-            })
             
+            if animated {
+                photoImage.alpha = 0
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.photoImage.alpha = 1.0
+                    self.spinner.alpha = 0
+                }, completion: { _ in
+                    self.spinner.stopAnimating()
+                })
+            }
         } else {
             photoImage.image = nil
             photoImage.alpha = 0
